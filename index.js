fc2c19452a07004481226776dd39300f0ceeeacd
Nitro Development <66451996+Nitr0Dev@users.noreply.github.com>
2021-01-22 17:08:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//24/7 Hosting Script 
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('</> By Nitro#0117 w/ ❤'));
+
+app.listen(port, () => console.log(`This app is listining on http://localhost:${port}`));
+
+// Bot Code 
 const fs = require('fs');
 
 const Discord = require('discord.js');
